896b937fb2fcc88516bec89cbc1e5fc892e5e201
sayfullayev0204 <sayfullayevgayrat7@gmail.com>
2024-11-24 13:01:25 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d@(vl_xd3ee&ny)wpcg+d-cbj1@2c$#%28)el@@baoa&ousv+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app',"*"]
 
 
 # Application definition
